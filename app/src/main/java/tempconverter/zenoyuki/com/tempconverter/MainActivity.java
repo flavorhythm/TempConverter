67f620ebbb6f7de7b51d12d65a372cc0c0997f63
flavorhythm <zeyuki@gmail.com>
2015-11-05 14:29:15 -0600

    Wrote core functionality

--- app/src/main/java/tempconverter/zenoyuki/com/tempconverter/MainActivity.java
+++ app/src/main/java/tempconverter/zenoyuki/com/tempconverter/MainActivity.java
@@ -5,11 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText cVal, fVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,66 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        cVal = (EditText)findViewById(R.id.c_ID);
+        fVal = (EditText)findViewById(R.id.f_ID);
+
+        final DecimalFormat valFormat = new DecimalFormat("###.#");
+
+        cVal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(cVal.isFocused()) {
+                    try {
+                        double fValInt = convert(Double.parseDouble(s.toString()), 'f');
+                        String output = valFormat.format(fValInt);
+
+                        fVal.setText(output);
+                    } catch (NumberFormatException e) {
+                        fVal.setText("");
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        fVal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(fVal.isFocused()) {
+                    try {
+                        double cValInt = convert(Double.parseDouble(s.toString()), 'c');
+                        String output = valFormat.format(cValInt);
+
+                        cVal.setText(output);
+                    } catch (NumberFormatException e) {
+                        cVal.setText("");
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+    }
+
+    private double convert(double val, char conv_to) {
+        switch (conv_to) {
+            case 'f':
+                return val * (9.0/5.0) + 32.0;
+            case 'c':
+                return (val - 32.0) * (5.0/9.0);
+            default:
+                return 0;
+        }
     }
 
     @Override
